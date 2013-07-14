40b5cbbd7caf1f970a43405413ea7e2d323fc821
lamerw <bel-azar@ya.ru>
2013-07-14 06:47:20 +0200

    Minor fix for api links

--- static/js/services.js
+++ static/js/services.js
@@ -9,7 +9,7 @@
 /* Services */
 angular.module('expenseShareServices', ['ngResource'])
   .factory('Event', function ($resource, $rootScope) {
-    var Event =  $resource($rootScope.config.basePath + '/event/:eventId', {}, {
+    var Event =  $resource($rootScope.config.basePath + 'event/:eventId', {}, {
       query: {method: 'GET', cache: true, params: {}, isArray: true},
       get: {method: 'GET', cache: true, params: {eventId:''}, isArray: false},
       patch: {method: 'PATCH', params: {eventId:''}}
